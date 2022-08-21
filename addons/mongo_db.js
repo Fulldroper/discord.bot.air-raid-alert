0d50b63664b9fc65b9f555833c83563c7d4c2296
full_droper <full_droper@pm.me>
2022-08-22 02:32:29 +0300

    'upd'

--- addons/mongo_db.js
+++ addons/mongo_db.js
@@ -11,8 +11,13 @@ module.exports = class {
   }
 
   async connect() {
+    try {
+      this.connection = await this.client.connect()
+    } catch (error) {
+      console.log(error);
+      return
+    }
     console.log("[mongodb]: conected");
-    this.connection = await this.client.connect()
     this.pipe = await this.connection.db(this.db_name).collection(this.collection_name)
 
     return this.connection

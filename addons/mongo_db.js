8cb70b9f506ee247285c8ed5b0565b0a9ce193cd
full_droper <full_droper@pm.me>
2022-08-22 02:18:50 +0300

    'upd'

--- addons/mongo_db.js
+++ addons/mongo_db.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 
 module.exports = class {
   constructor (url, db, collection) {
-    this.client = new(MongoClient)(url, {
+    this.client = new MongoClient(url, {
       authSource: "admin",
       useNewUrlParser: true
     });
@@ -11,8 +11,8 @@ module.exports = class {
   }
 
   async connect() {
-    this.connection = await this.client.connect()
     console.log("[mongodb]: conected");
+    this.connection = await this.client.connect()
     this.pipe = await this.connection.db(this.db_name).collection(this.collection_name)
 
     return this.connection

4e344d5afe7d4d06398e6f62e4065d6058047bd5
full_droper <full_droper@pm.me>
2023-02-25 11:43:06 +0200

    init

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,18 @@ const r = require("axios")
 module.exports = async function () {
   this.db.connect()
   let users_counter = 0
-  const interval = 20000
+  const interval = 13000
+
+  const req = {
+    method: 'get',
+    url: 'http://alerts.com.ua/api/states',
+    maxRedirects: 0,
+    headers: {
+      "X-API-Key": process.env.API_KEY
+    }
+  }
+
+  const Time = x => new Date(`${x}`).getTime()
   
   await this.guilds.cache.forEach(async s => users_counter += s.memberCount )
   
@@ -15,63 +26,47 @@ module.exports = async function () {
   console.log(`[Users](${users_counter})`);
 
   while (true) {
-    console.log(`[checking updates by API]`);
-    let { latest, alerts: older} = await this.db.get(`${this.user.username}:buffer`) || { latest: false, alerts: []}
-    const req = {
-      method: 'get',
-      url: 'https://api.alerts.in.ua/v1/alerts/active.json',
-      maxRedirects: 0,
-      headers: {
-        "Connection": "closed"
-      }
-    }
-    latest && (req.headers["If-Modified-Since"] = latest)
-    const { status, data, headers } = await r(req).catch(() => console.log(`[updates not found]`)) || {status: false, data: false, headers: false}
-    let { alerts: newer } = data
-    if (status === 200) {
-      console.log(`[updates found]`);
-      
-      for (let n = 0; n < newer.length; n++) {
-        let o = older.findIndex(old_region => old_region.location_oblast !== newer[n].location_oblast)
-
-        if(o > 0){
-          newer.splice(1, n)
-          older.splice(1, o)
-        }
-        
-        console.log({newer, older});
-      }      
-      // send to start
-      for (const region of newer) {
-        if(region["location_type"] === "oblast") {
-          const subscribers = await this.db.get(`${this.user.username}:${region.location_oblast}`) || []
-          for (const uid of subscribers) {
-            const user = await this.users.fetch(uid) || false
-            if (user) {
-              if (user.bot) return
-              user.send(`🚨 ${region.location_title} повітряна тривога`)
-              .catch(() => console.log(`${user.tag} not sended`))        
+    console.log(`checking updates by API`);
+    // get from db
+    const { states = [], last_update = false } = (await this.db.get(`${this.user.username}:buffer`)) || {}
+    const { data } = await r(req).catch(() => console.log(`rate limit`)) || { data: { states: [], last_update: false }}
+    // check update time
+    if(Time(data.last_update) > Time(last_update)){
+      // format old
+      const old = {}
+      states.forEach(e => old[e.name] = {alert: e.alert, changed: Time(e.changed)})
+      // format now
+      const now = {}
+      data.states.forEach(e => now[e.name] = {alert: e.alert, changed: Time(e.changed)})
+      // find all new
+      for (const key in now) {
+        // check if state changed
+        if (now[key].changed > old[key].changed) {
+          // get subscribers list
+          const subscribers = await this.db.get(`${this.user.username}:${key}`) || []
+          // send to subscribers status
+          for (const subscriber of subscribers) {
+            const user = await this.users.fetch(subscriber) || false
+            // check if user founded and not bot
+            if (user && !user.bot) {
+              // send state to user              
+              user.send(`${
+                now[key].alert ? '🚨' : '🕊️'
+              } ${key} ${
+                now[key].alert ? 'повітряна тривога' : 'відбій повітряної тривоги'
+              }`)
+              .catch(() => console.log(`${user.tag} not sended`)) 
             }
           }
-        }
+        } else console.log(`${key} not changed`);        
       }
-      // send to end
-      for (const region of older) {
-        if(region["location_type"] === "oblast") {
-          const subscribers = await this.db.get(`${this.user.username}:${region.location_oblast}`) || []
-          for (const uid of subscribers) {
-            const user = await this.users.fetch(uid) || false
-            if (user) {
-              if (user.bot) return
-              user.send(`🕊️ ${region.location_title} відбій повітряної тривоги`)
-              .catch(() => console.log(`${user.tag} not sended`))        
-            }
-          }
-        }
-      }
-      
-      await this.db.set(`${this.user.username}:buffer`, { latest: headers["last-modified"], alerts: newer})
-    }
+      // save new date
+      await this.db.set(`${this.user.username}:buffer`, data)
+    } else if(last_update === false) {
+      console.log('init update');
+      await this.db.set(`${this.user.username}:buffer`, data)
+    } else console.log(`updates not found`);
+    // wait gateway interval    
     await interval.sleep()
   }
 }
